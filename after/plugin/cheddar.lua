0a658ae50f1c69bfebe40e6e29220187d99434e1
kolbbond <kolbbond@gmail.com>
2024-05-01 21:27:52 +0000

    modified cheddar after

--- after/plugin/cheddar.lua
+++ after/plugin/cheddar.lua
@@ -1,3 +1,6 @@
-print('loading cheddar');
-require('cheddar').setup();
+-- print('loading cheddar');
+--require('cheddar').setup();
+--require('cheddar').test();
+ch = require('cheddar').setup();
+ch.test();
 
