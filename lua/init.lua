c52a4c4b996bacc662e026874aad5aa77b6f635d
kolbbond <kolbbond@gmail.com>
2024-08-13 15:57:35 -0400

    netrw relative line numbers

--- lua/init.lua
+++ lua/init.lua
@@ -7,6 +7,9 @@ vim.opt_local.formatoptions:remove({ 'r', 'o'})
 -- slime
 vim.g.slime_target = "tmux"
 
+-- netrw line numbers
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
+
 -- change coloring
 -- require("colors")
 
