ff9332c7cc45ff4a3d24e77581b6174744fcc2ef
kolbbond <kolbbond@gmail.com>
2025-01-06 11:28:06 -0500

    merge

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -103,6 +103,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("n", "<C-q>", function() vim.lsp.buf.signature_help() end, opts)
+
 end);
 
 -- remap to switch between source and header files
